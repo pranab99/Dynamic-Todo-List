63b0617b8cb6aa0fb195a42cebc6f1011f497fa7
pranab1999 <impranabdas99@gmail.com>
2021-08-05 02:31:15 +0530

    Added all files

---
+++ app.js
@@ -0,0 +1,38 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app= express();
+
+var items=["Play Chess","Learn Web Dev","Learn Data Structures"];
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.get("/",function(req,res){
+  
+
+    var today=new Date();
+    var options = { weekday: 'long', month: 'long', day: 'numeric' };
+    var day=today.toLocaleDateString("en-US",options);
+
+
+    res.render("list",{
+     kindOfDay: day,
+     newListItems: items
+    });
+
+
+});
+
+app.post("/",function(req,res){
+
+   var item= req.body.newItem;
+   items.push(item);
+   res.redirect("/");
+    
+
+});
+
+app.listen(3000,function(){
+    console.log("Server started at port 3000");
+
+});
