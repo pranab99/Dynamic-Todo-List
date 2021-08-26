b7d317abcf449bb84746fcb1929d801d8a7024ac
pranab1999 <impranabdas99@gmail.com>
2021-08-27 01:15:08 +0530

    added work

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const express=require("express");
 const bodyParser=require("body-parser");
+const date= require(__dirname+"/date.js");
 
 const app= express();
 
-var items=["Play Chess","Learn Web Dev","Learn Data Structures"];
+let items=["Play Chess","Learn Web Dev","Learn Data Structures"];
+let workItems=[];
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.get("/",function(req,res){
-  
-
-    var today=new Date();
-    var options = { weekday: 'long', month: 'long', day: 'numeric' };
-    var day=today.toLocaleDateString("en-US",options);
-
+   
+    let day=date;   
 
     res.render("list",{
-     kindOfDay: day,
+     listTitle: day,
      newListItems: items
     });
 
@@ -24,15 +23,38 @@ app.get("/",function(req,res){
 });
 
 app.post("/",function(req,res){
-
-   var item= req.body.newItem;
-   items.push(item);
+  
+   let item= req.body.newItem; 
+   if(req.body.list === "Work") {
+   workItems.push(item);
+   res.redirect("/work");
+   }
+   else{
+        items.push(item);
    res.redirect("/");
     
 
+   }
+
 });
 
-app.listen(process.env.PORT||3000,function(){
-    console.log("Server started at port 3000");
+app.get("/work",function(req,res){
+     res.render("list",{                         //here list is the list.ejs file
+         listTitle: "Work List",
+         newListItems: workItems
+    });
+});
+
+app.get("/about",function(req,res){
+    res.render("about"); //rendering about.ejs
+
+
+
+});
+
+
+
+app.listen(process.env.PORT||4000,function(){
+    console.log("Server started at port 4000");
 
 });
