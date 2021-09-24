0677870ea4034ae8e103e85e44b64506ac77e595
pranab1999 <impranabdas99@gmail.com>
2021-09-25 02:23:08 +0530

    Added delete route

--- app.js
+++ app.js
@@ -1,40 +1,96 @@
 const express=require("express");
 const bodyParser=require("body-parser");
-const date= require(__dirname+"/date.js");
+
+const mongoose=require("mongoose");
 
 const app= express();
-
-let items=["Play Chess","Learn Web Dev","Learn Data Structures"];
-let workItems=[];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true});
+
+const itemsSchema =new mongoose.Schema({
+    _id:mongoose.Schema.ObjectId,
+    name:String
+});
+
+const Item= mongoose.model("Item",itemsSchema); //singular version "Item" , btw this is the model name
+Item._id = mongoose.Types.ObjectId('000000000000000000000001');
+
+
+const item1=new Item({
+  
+    name:"Welcome to Pranab's todo list!!!"
+});
+const item2=new Item({
+  
+    name:"Hit the + Button to add a new item"
+});
+const item3=new Item({
+   
+    name:"<-----Hit this to delete an item"
+});
+
+const defaultItems=[item1,item2,item3];
+
+
+
+
 app.get("/",function(req,res){
    
-    let day=date;   
+Item.find({},function(err,foundItems){
 
-    res.render("list",{
-     listTitle: day,
-     newListItems: items
+if(foundItems.length === 0){
+             Item.insertMany(defaultItems,function(err){
+    if(err)
+    console.log(err);
+    else
+    console.log("Successfully saved default items to database");
+
+});
+
+res.redirect("/");
+}
+
+else{
+     res.render("list",{   // this list is the list.ejs file not the button
+     listTitle: "Today",
+     newListItems: foundItems
     });
 
+    }
+});
 
 });
 
 app.post("/",function(req,res){
   
-   let item= req.body.newItem; 
-   if(req.body.list === "Work") {
-   workItems.push(item);
-   res.redirect("/work");
-   }
-   else{
-        items.push(item);
-   res.redirect("/");
-    
+   let itemName= req.body.newItem;
+   const item=new Item({
+    name:itemName,
+    _id: mongoose.Types.ObjectId()   
 
-   }
+   }); 
+
+   item.save();
+   res.redirect("/");
+
+});
+
+app.post("/delete",function(req,res){
+   const checkedItemId = req.body.checkbox;
+   console.log(checkedItemId);
+   Item.findByIdAndRemove(checkedItemId,function(err){
+       if(!err){
+           console.log("Successfully deleted checked item.");
+           res.redirect("/");
+       }
+
+
+   });
+   
 
 });
 
@@ -54,7 +110,7 @@ app.get("/about",function(req,res){
 
 
 
-app.listen(process.env.PORT||4000,function(){
+app.listen(process.env.PORT||3006,function(){
     console.log("Server started at port 4000");
 
 });
